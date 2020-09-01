37fe315e7a35972897afb65c57ba56844a99e636
GiulianaSanchez <69353305+GiulianaSanchez@users.noreply.github.com>
2020-09-01 10:26:25 -0400

    Create Entero_Positivo_n_ciclos.cpp

---
+++ Entero_Positivo_n_ciclos.cpp
@@ -0,0 +1,20 @@
+/******************************************************************************
+
+Elaborar un programa que pida al usuario un entero positivo n. Luego, el programa
+debe pedir al usuario n enteros usando un ciclo. El programa deberá encontrar el
+mayor, menor, y promedio de los valores dados por el usuario e imprimirlos al final.
+
+Entrada: n y enteros
+Salida: mayor menor promedio
+
+*******************************************************************************/
+
+#include <iostream>
+#include "conio.h"
+
+using namespace std;
+
+int main ()
+{
+    
+}
